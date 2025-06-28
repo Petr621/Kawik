0924cf68e6a9cb59dec17ec75a973fe347bb5093
Zozick <61080628+Petr621@users.noreply.github.com>
2025-06-28 03:32:13 +0300

    Add files via upload

---
+++ update.bat
@@ -0,0 +1,27 @@
+@echo off
+
+git checkout -- requirements.txt
+
+git stash
+if not %errorlevel%==0 (
+    echo "Could not successfully stash current changes"
+    exit /b 1
+)
+
+git pull --rebase origin master
+if not %errorlevel%==0 (
+    echo "Could not successfully pull the latest changes to your local repository"
+    exit /b 1
+)
+
+git stash pop
+if not %errorlevel%==0 (
+    echo "Could not successfully pop the stash"
+    exit /b 1
+)
+
+pip install -r requirements.txt
+if not %errorlevel%==0 (
+    echo "Could not successfully install packages"
+    exit /b 1
+)
